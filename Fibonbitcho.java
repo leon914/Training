9cb1e4557c828d8d8bbe2524c390d5fc2dd6ed79
Your Name <you@example.com>
2017-05-25 10:13:49 +0100

    Even Fibonacci Numbers

---
+++ Fibonbitcho.java
@@ -0,0 +1,19 @@
+public class Fibonbitcho {
+
+  public static void main(String[] args) {
+    System.out.println("Answer:" + fibobisho());
+  }
+
+  public static int fibobisho() {
+    int total = 0;
+    int i = 0;
+    do {
+      if ((i & 1) == 0) {
+        total = total + i;
+      }
+      System.out.println(total)
+      i++;
+    } while (total <= 4000000);
+    return total;
+  }
+}
